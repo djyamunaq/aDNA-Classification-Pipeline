d8f1dfbbf63d58f3f50a534742d43b220a0b0dea
djyamunaq <dy.xcvi@gmail.com>
2024-02-28 13:18:43 +0100

    Feature: Add --saveBam flag

--- classpipe/__main__.py
+++ classpipe/__main__.py
@@ -3,6 +3,11 @@ from colorama import Fore, Back, Style
 import subprocess
 import os
 
+def printErrorMessage(message):
+    print(Fore.RED + Style.BRIGHT + '[ERROR]', end=' ')
+    print(message)
+    print(Style.RESET_ALL)
+
 def printRunningMessage(toolName):
     print(Fore.BLUE + Style.BRIGHT + '> Running on', toolName)
     print(Style.RESET_ALL)
@@ -31,6 +36,7 @@ def main():
     parser.add_argument('--damageProfiler', action='store_true', help='Run data on damageProfiler')
     parser.add_argument('--atlas', action='store_true', help='Run data on Atlas')
     parser.add_argument('--metaDamage', action='store_true', help='Run data on metaDamage')
+    parser.add_argument('--saveBam', action='store_true', help='Save generated BAM and BAI files to output dir')
 
     # Set output destionation from command line
     parser.add_argument('--output', help='Set output destination [Default: .]', default='.')
@@ -38,43 +44,59 @@ def main():
     # Get arguments from command line
     args: Namespace = parser.parse_args()
 
-    input_sam_file_path = os.path.join(os.path.dirname(__file__),'.data/DNA_input.sam')
-    input_bam_file_path = os.path.join(os.path.dirname(__file__),'.data/DNA_input.bam')
-    
-    # Check if file DNA inputs are in fastq format 
-    if args.aDNA1.endswith('.fq') or args.aDNA1.endswith('.fastq'):
+    # Create output dir if it doesn't exist
+    output_dir = args.output
+    if args.output == '.':
+        output_dir = os.path.join(args.output, 'class_pipe_output')
+
+    bam_dir = os.path.join(os.path.dirname(__file__), '.data')
+    input_sam_file_path = os.path.join(bam_dir, 'DNA_input.sam')
+    input_bam_file_path = os.path.join(bam_dir, 'DNA_input.bam')        
+
+    # Save BAM and BAI to output
+    if args.saveBam:
+        bam_dir = os.path.join(output_dir, 'BAM')
+        subprocess.run(['rm', '-rf', bam_dir])
+        subprocess.run(['mkdir', bam_dir])
+        input_sam_file_path = os.path.join(bam_dir, 'DNA_input.sam')
+        input_bam_file_path = os.path.join(bam_dir, 'DNA_input.bam')
+
+
+    # Check if file DNA inputs are in a valid format 
+    if args.aDNA1.endswith('.fq') or args.aDNA1.endswith('.fastq') or args.aDNA1.endswith('.fq.gz') or args.aDNA1.endswith('.fastq.gz'):
         # Convert reference DNA file (.fa) + aDNA files (.fq) to .sam format
         if args.aDNA2:
             subprocess.run(['minimap2', '-t', '8', '-a', '-x', 'sr', args.refDNA, args.aDNA1, args.aDNA2, '-o', input_sam_file_path])
         else:
             subprocess.run(['minimap2', '-t', '8', '-a', '-x', 'sr', args.refDNA, args.aDNA1, '-o', input_sam_file_path])
         subprocess.run(['samtools', 'view', '-b', input_sam_file_path, '-o', input_bam_file_path])
-    else:
+        subprocess.run(['rm', '-rf', input_sam_file_path])
+    elif args.aDNA1.endswith('.bam') or args.aDNA1.endswith('.sam'):
         # Move .bam/.sam file to .data directory
         if args.aDNA1.endswith('.bam'):
             input_bam_file_path = os.path.abspath(args.aDNA1)
         elif args.aDNA1.endswith('.sam'):
             input_sam_file_path = os.path.abspath(args.aDNA1)
             subprocess.run(['samtools', 'view', '-b', input_sam_file_path, '-o', input_bam_file_path])
+            subprocess.run(['rm', '-rf', input_sam_file_path])
+    else:
+        message = 'Provide a valid format for the input DNA (.fq, .bam, .sam)'
+        printErrorMessage(message)
 
     # Add MD tag
-    input_bam__aligned_file_path = os.path.join(os.path.dirname(__file__),'.data/DNA_input_aligned.bam')
-    input_bam_aligned_file = open(input_bam__aligned_file_path, '+w')
+    input_bam_aligned_file_path = os.path.join(bam_dir, 'DNA_input_aligned.bam')
+    input_bam_aligned_file = open(input_bam_aligned_file_path, '+w')
     subprocess.run(['samtools', 'calmd', '-b', input_bam_file_path, args.refDNA], stdout=input_bam_aligned_file)
     input_bam_aligned_file.close()
 
     # Sort reads
-    input_bam_aligned_sorted_file_path = os.path.join(os.path.dirname(__file__),'.data/DNA_input_aligned_sorted.bam')
-    subprocess.run(['samtools', 'sort', input_bam__aligned_file_path, '-o', input_bam_aligned_sorted_file_path])
+    input_bam_aligned_sorted_file_path = os.path.join(bam_dir, 'DNA_input_aligned_sorted.bam')
+    subprocess.run(['samtools', 'sort', input_bam_aligned_file_path, '-o', input_bam_aligned_sorted_file_path])
     
     # Index reads
-    subprocess.run(['samtools', 'index', input_bam_aligned_sorted_file_path])
-
-    # Create output dir if it doesn't exist
-    output_dir = args.output
-    if args.output == '.':
-        output_dir = os.path.join(args.output, 'class_pipe_output')
-
+    input_bam_index_file_path = os.path.join(bam_dir, 'DNA_input_aligned_sorted.bai')
+    subprocess.run(['samtools', 'index', input_bam_aligned_sorted_file_path, input_bam_index_file_path])
+    
     ##############################################
     # PMDTools ###################################
     ##############################################
@@ -128,7 +150,7 @@ def main():
         printEndOfToolMessage('mapDamage')
 
     ##############################################
-    # mapDamage ##################################
+    # pyDamage ###################################
     ##############################################
     if args.pyDamage:
         # Create output dir
@@ -161,7 +183,10 @@ def main():
     ##############################################
     if args.atlas:
         printRunningMessage('Atlas')
-        # subprocess.run([os.path.join(os.path.di rname(__file__), './atlas/atlas'), 'task=PMD', 'bam=', input_bam_file_path, 'fasta=', args.refDNA, 'lenght=50'])
+        # subprocess.run([os.path.join(os.path.dirname(__file__), './atlas/atlas'), '--help'])
+        atlas_output_dir_path = os.path.join(output_dir, 'atlas')
+        command = os.path.join(os.path.dirname(__file__), './atlas/atlas') + ' task=PMD' + ' bam=' + input_bam_file_path + ' fasta=' + args.refDNA + ' lenght=50' + ' out=' + atlas_output_dir_path
+        subprocess.run(command, shell=True)
         # ./atlas task=PMD bam=example.bam fasta=reference.fa length=50
     
     ##############################################
@@ -170,8 +195,11 @@ def main():
     if args.metaDamage:
         printRunningMessage('MetaDamage')
 
+    # Delete unused files
+    # subprocess.run(['rm', '-rf', input_bam_file_path])
+    # subprocess.run(['rm', '-rf', input_bam_aligned_file_path])
+    
     printEndOfPipelineMessage()
-
 
 
 if __name__ == '__main__':
